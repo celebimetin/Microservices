a3296e488691cf3aa68bdf455353c0c6e67cbb80
Metin Çelebi <metincelebi5534@gmail.com>
2022-11-12 16:01:06 +0300

    AutoMapper ile Dto sınıfları için mapping işlemi tamamlandı.

---
+++ Services/Catalog/Services.Catalog/Mappings/GeneralMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Services.Catalog.Dtos;
+using Services.Catalog.Models;
+
+namespace Services.Catalog.Mappings
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Course, CourseDto>().ReverseMap();
+            CreateMap<Course, CourseCreateDto>().ReverseMap();
+            CreateMap<Course, CourseUpdateDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Feature, FeatureDto>().ReverseMap();
+        }
+    }
+}
--- Services/Catalog/Services.Catalog/Startup.cs
+++ Services/Catalog/Services.Catalog/Startup.cs
@@ -22,10 +22,9 @@ namespace Services.Catalog
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddAutoMapper(typeof(Startup));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -33,7 +32,6 @@ namespace Services.Catalog
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
