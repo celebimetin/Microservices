4aeae4935b5d600e660dfae1cb8a81f894dc3fbf
Metin Çelebi <metincelebi5534@gmail.com>
2022-11-14 16:37:22 +0300

    Config dosyasında ApiResources, ApiScopes ve Clients kodlaması tamamlandı.

--- IdentityServer/IdentityServer/Startup.cs
+++ IdentityServer/IdentityServer/Startup.cs
@@ -39,11 +39,10 @@ namespace IdentityServer
                 options.Events.RaiseInformationEvents = true;
                 options.Events.RaiseFailureEvents = true;
                 options.Events.RaiseSuccessEvents = true;
-
-                // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
                 options.EmitStaticAudienceClaim = true;
             })
                 .AddInMemoryIdentityResources(Config.IdentityResources)
+                .AddInMemoryApiResources(Config.ApiResources)
                 .AddInMemoryApiScopes(Config.ApiScopes)
                 .AddInMemoryClients(Config.Clients)
                 .AddAspNetIdentity<ApplicationUser>();
