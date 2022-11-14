0ac170bbd58e0742c1bf9478543318f581aae9bd
Metin Çelebi <metincelebi5534@gmail.com>
2022-11-14 16:11:56 +0300

    SignUp endpoint kodlaması tamamlandı.

---
+++ IdentityServer/IdentityServer/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using IdentityServer.Dtos;
+using IdentityServer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Dtos;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpDto response)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = response.UserName,
+                Email = response.Email,
+                City = response.City
+            };
+
+            var result = await _userManager.CreateAsync(user, response.Password);
+            if (!result.Succeeded)
+            {
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), 400));
+            }
+            return NoContent();
+        }
+    }
+}
