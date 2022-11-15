b4a1a15c7437dcb0a82a087018d80590daec7116
Metin Çelebi <metincelebi5534@gmail.com>
2022-11-15 10:57:07 +0300

    PhotoStockAPI için proje oluşturuldu.

--- IdentityServer/IdentityServer/Config.cs
+++ IdentityServer/IdentityServer/Config.cs
@@ -1,5 +1,6 @@
 ﻿using IdentityServer4;
 using IdentityServer4.Models;
+using System;
 using System.Collections.Generic;
 
 namespace IdentityServer
@@ -16,6 +17,16 @@ namespace IdentityServer
         public static IEnumerable<IdentityResource> IdentityResources =>
                    new IdentityResource[]
                    {
+                       new IdentityResources.Email(),
+                       new IdentityResources.OpenId(),
+                       new IdentityResources.Profile(),
+                       new IdentityResource()
+                       {
+                           Name="roles",
+                           DisplayName = "Roller",
+                           Description="Kullanıcı rolleri",
+                           UserClaims= new[] {"roles"}
+                       }
                    };
 
         public static IEnumerable<ApiScope> ApiScopes =>
@@ -35,7 +46,33 @@ namespace IdentityServer
                     ClientId = "WebMvcClient",
                     ClientSecrets = { new Secret("secret".Sha256()) },
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    AllowedScopes = { "catalog_fullpermission", "photo_stock_fullpermission", IdentityServerConstants.LocalApi.ScopeName }
+                    AllowedScopes =
+                    {
+                        "catalog_fullpermission",
+                        "photo_stock_fullpermission",
+                        IdentityServerConstants.LocalApi.ScopeName
+                    }
+                },
+                new Client
+                {
+                    ClientName = "Asp.NET Core Mvc",
+                    ClientId = "WebMvcClientForUser",
+                    AllowOfflineAccess = true,
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.Email,
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.OfflineAccess,
+                        IdentityServerConstants.LocalApi.ScopeName,
+                        "roles"
+                    },
+                    AccessTokenLifetime = 1*60*60,
+                    RefreshTokenExpiration = TokenExpiration.Absolute,
+                    AbsoluteRefreshTokenLifetime = (int)(DateTime.Now.AddDays(60)-DateTime.Now).TotalSeconds,
+                    RefreshTokenUsage = TokenUsage.ReUse
                 }
             };
     }
--- IdentityServer/IdentityServer/Startup.cs
+++ IdentityServer/IdentityServer/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using IdentityServer.Services;
 
 namespace IdentityServer
 {
@@ -48,17 +49,13 @@ namespace IdentityServer
                 .AddInMemoryClients(Config.Clients)
                 .AddAspNetIdentity<ApplicationUser>();
 
-            // not recommended for production - you need to store your key material somewhere secure
             builder.AddDeveloperSigningCredential();
+            builder.AddResourceOwnerValidator<IdentityResourceOwnerPasswordValidator>();
 
             services.AddAuthentication()
                 .AddGoogle(options =>
                 {
                     options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
-                    
-                    // register your IdentityServer with Google at https://console.developers.google.com
-                    // enable the Google+ API
-                    // set the redirect URI to https://localhost:5001/signin-google
                     options.ClientId = "copy client ID from Google here";
                     options.ClientSecret = "copy client secret from Google here";
                 });
