e26f973b4ab519d0ac669e3c1a47f0bea5f45194
Metin Çelebi <metincelebi5534@gmail.com>
2022-11-25 17:42:50 +0300

    Startup / Cookie settings kodlaması tamamlandı başarılı bir şekilde signIn işlemi yapıldı. cookie oluştu.

--- Frontends/WebApplication/Startup.cs
+++ Frontends/WebApplication/Startup.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 using WebApplication.Models;
 using WebApplication.Services;
 using WebApplication.Services.Interfaces;
@@ -24,6 +26,15 @@ namespace WebApplication
             services.AddHttpClient<IIdentityService, IdentityService>();
             services.Configure<ServiceApiSettings>(Configuration.GetSection("ServiceApiSettings"));
             services.Configure<ClientSettings>(Configuration.GetSection("ClientSettings"));
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+            {
+                options.LoginPath = "/Auth/SignIn";
+                options.Cookie.Name = "Cookie";
+                options.ExpireTimeSpan = System.TimeSpan.FromDays(30);
+                options.SlidingExpiration = true;
+            });
+
             services.AddControllersWithViews();
         }
 
@@ -41,7 +52,7 @@ namespace WebApplication
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
