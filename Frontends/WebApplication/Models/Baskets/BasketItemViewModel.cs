6c8b9a2f27c7c756fc603305fab718ded5a0280d
Metin Çelebi <metincelebi5534@gmail.com>
2022-12-09 15:59:09 +0300

    BasketItemViewModel ve BasketViewModel class'ları kodlaması tamamlandı.

---
+++ Frontends/WebApplication/Models/Baskets/BasketItemViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication.Models.Baskets
+{
+    public class BasketItemViewModel
+    {
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+        private decimal? DiscountAppliedPrice { get; set; }
+
+        public decimal GetCurrentPrice { get => DiscountAppliedPrice != null ? DiscountAppliedPrice.Value : Price; }
+
+        public void AppliedDiscount(decimal discountPrice)
+        {
+            DiscountAppliedPrice = discountPrice;
+        }
+    }
+}
